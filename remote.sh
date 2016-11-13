a6280664e372c68677d44dcbf1c835549dbe5488
Benjamin Wang <baominw@baominw-m02.vmware.com>
2016-11-13 13:59:34 +0800

    Add remote exec utility

---
+++ remote.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+USER='root'
+PASSWORD='ca$hc0w'
+
+while getopts u:p: option
+do
+    case $option in
+    u)
+        USER=$OPTARG
+        ;;
+    p)
+        PASSWORD=$OPTARG
+        ;;
+    \?)
+        echo "remote.sh -u username -p 'password' hostname 'command'"
+        exit;
+        ;;
+    esac
+done
+
+echo $OPTIND
+shift $[$OPTIND-1]
+
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -l $USER $1 $2
